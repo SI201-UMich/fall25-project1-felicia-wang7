1b9f293bcbe5e162c452e2bc4f3eb9044038a2f8
Felicia Wang <wangfeli@umich.edu>
2025-10-15 16:58:39 -0400

    Initial commit

---
+++ felicia_tests.py
@@ -0,0 +1,12 @@
+# Name: Felicia Wang
+# ID: 62645970
+# Email: wangfeli@umich.edu
+# Collaborators: Huy Pham, John (Yohan) Park
+
+import unittest
+
+from felicia_project import (
+    percent_fem_bill_over_40_by_island,
+    percent_male_flip_under_180_by_species,
+)
+
