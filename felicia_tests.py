1692a57c1b7a0919bc535d4b8f7a8f4c00c7d4d5
Felicia Wang <wangfeli@umich.edu>
2025-10-15 17:00:18 -0400

    Implemented TestFeliciaFunctionB

--- felicia_tests.py
+++ felicia_tests.py
@@ -49,3 +49,45 @@ class TestFeliciaFunctionA(unittest.TestCase):
         ]
         res = percent_fem_bill_over_40_by_island(data)
         self.assertNotIn("Dream", res)
+
+class TestFeliciaFunctionB(unittest.TestCase):
+    """Tests for percent_male_flip_under_180_by_species"""
+
+    def test_usual_mixed_counts(self):
+        data = [
+            {"sex":"MALE","flipper_length_mm":175.0,"species":"Adelie"},
+            {"sex":"MALE","flipper_length_mm":185.0,"species":"Adelie"},
+            {"sex":"MALE","flipper_length_mm":170.0,"species":"Chinstrap"},
+        ]
+        res = percent_male_flip_under_180_by_species(data)
+        self.assertAlmostEqual(res["Adelie"], 50.0)
+        self.assertAlmostEqual(res["Chinstrap"], 100.0)
+
+    def test_boundary_equals_180_not_included(self):
+        data = [
+            {"sex":"MALE","flipper_length_mm":180.0,"species":"Gentoo"},
+            {"sex":"MALE","flipper_length_mm":179.9,"species":"Gentoo"},
+            {"sex":"MALE","flipper_length_mm":200.0,"species":"Gentoo"},
+        ]
+        res = percent_male_flip_under_180_by_species(data)
+        self.assertAlmostEqual(res["Gentoo"], (1/3)*100)
+
+    def test_missing_numeric_skipped(self):
+        data = [
+            {"sex":"MALE","flipper_length_mm":None,"species":"Adelie"},
+            {"sex":"MALE","flipper_length_mm":150.0,"species":"Adelie"},
+            {"sex":"MALE","flipper_length_mm":190.0,"species":"Adelie"},
+        ]
+        res = percent_male_flip_under_180_by_species(data)
+        self.assertAlmostEqual(res["Adelie"], 50.0)
+
+    def test_no_valid_male_measurements_group_absent(self):
+        data = [
+            {"sex":"MALE","flipper_length_mm":None,"species":"Chinstrap"},
+            {"sex":"FEMALE","flipper_length_mm":150.0,"species":"Chinstrap"},
+        ]
+        res = percent_male_flip_under_180_by_species(data)
+        self.assertNotIn("Chinstrap", res)
+
+if __name__ == "__main__":
+    unittest.main()
